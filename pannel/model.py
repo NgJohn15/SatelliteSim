1556b62c4ac3104aa3db2127967260b8e12a8834
Xeather <ngjohn15@gmail.com>
2021-07-15 13:31:13 -0600

    Added yolo custom model

--- pannel/model.py
+++ pannel/model.py
@@ -3,5 +3,5 @@ from imageai.Detection.Custom import DetectionModelTrainer
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="ports")
-trainer.setTrainConfig(object_names_array=["aux, dc, usb"], batch_size=4, num_experiments=200, train_from_pretrained_model="pretrained-yolov3.h5")
+trainer.setTrainConfig(object_names_array=["aux", "dc", "usb"], batch_size=4, num_experiments=20, train_from_pretrained_model="pretrained-yolov3.h5")
 trainer.trainModel()
