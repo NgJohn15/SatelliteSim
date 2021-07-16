e2a32ca78a195c2ca11e327a3f06dcadc9224683
Xeather <ngjohn15@gmail.com>
2021-07-16 13:24:47 -0600

    updated predict script to take custom object detection model

--- pannel/yolo_predict.py
+++ pannel/yolo_predict.py
@@ -1,20 +1,27 @@
-from imageai.Detection import ObjectDetection
+from imageai.Detection.Custom import CustomObjectDetection
 import os
 
+model_name = "ports/models/detection_model-ex-012--loss-0009.301.h5"
 execution_path = os.getcwd()
 
-detector = ObjectDetection()
+detector = CustomObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
+detector.setModelPath(os.path.join(execution_path, model_name))
+detector.setJsonPath(os.path.join(execution_path, "ports/json/detection_config.json"))
 detector.loadModel()
 
-detections, objects_path = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, "image3.jpg"),
-                                                           output_image_path=os.path.join(execution_path,
-                                                                                          "image3new.jpg"),
-                                                           minimum_percentage_probability=30,
-                                                           extract_detected_objects=True)
+filepath = "dataset/images/test/"
+filename = "files.txt"
 
-for eachObject, eachObjectPath in zip(detections, objects_path):
-    print(eachObject["name"], " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"])
-    print("Object's image saved in " + eachObjectPath)
-    print("--------------------------------")
+# read list of files
+imagePaths = open(filepath + filename).read().strip().split("\n")
+
+for imagePath in imagePaths:
+    test_img = imagePath
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, filepath + test_img),
+                                                 output_image_path=os.path.join(execution_path,
+                                                filepath+"/result/" + test_img),
+                                                 minimum_percentage_probability=50)
+    print(imagePath)
+    for detection in detections:
+        print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
